09f63955c5baa16542c00210d036effc4a3af1fd
DELL <DELL@windows10.microdone.cn>
2019-10-23 18:26:57 +0800

    编写查询所有的实现方法

--- src/com/dao/UserDaoImpl.java
+++ src/com/dao/UserDaoImpl.java
@@ -1,10 +1,19 @@
 package com.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class UserDaoImpl implements UserDao {
 
 	@Override
 	public void select() {
-		// TODO Auto-generated method stub
+		//连接对象
+		Connection conn = null;
+		//执行对象
+		PreparedStatement pstmt = null;
+		//货车
+		ResultSet rs = null;
 		
 	}
 
